b32ff0646fe59140d0c912ef3d1ab620a8054740
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-10-02 15:35:25 +0700

    update daily-dropped-voice-calls: add month and week

---
+++ assets/directives/tr-datepicker-week.directive.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    window.app
+        .directive('trDatepickerWeek', trDatepickerWeek);
+
+    trDatepickerWeek.$inject = [];
+    function trDatepickerWeek() {
+        // Usage:
+        // Set ng-model with week datepicker jquery UI.
+        // Creates:
+        // Require jquery UI.
+        var directive = {
+            link: link,
+            restrict: 'A',
+            scope: {
+                dateFrom: '=',
+                dateTo: '='
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            element.on('change', () => {
+                scope.$apply(() => {
+                    scope.dateFrom = moment(element.val()).startOf('week').toDate();
+                    scope.dateTo = moment(element.val()).endOf('week').toDate()
+                });
+            });
+        }
+    }
+})();
